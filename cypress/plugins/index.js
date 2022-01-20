0f79b6a27db2567826a35d0435d0816e427b2045
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-01-20 11:33:04 -0500

    adding cypress-grep for internal testing

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,6 +20,9 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
+  // include this plugin before cypress-grep
+  // so if we find the test tags in the pull request body
+  // we can grep for them by setting the grep config
   require('../../src')(on, config, {
     // let's take this repo
     owner: 'bahmutov',
@@ -27,4 +30,11 @@ module.exports = (on, config) => {
     commit: process.env.GITHUB_SHA,
     token: process.env.GITHUB_TOKEN || process.env.PERSONAL_GH_TOKEN,
   })
+
+  // https://github.com/bahmutov/cypress-grep
+  require('cypress-grep/src/plugin')(config)
+
+  // cypress-grep could modify the config (the list of spec files)
+  // thus it is important to return the modified config to Cypress
+  return config
 }
