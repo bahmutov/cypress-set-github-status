f1951186b6b07f5c1bd706887f48f22de55ed222
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-09-08 12:41:36 -0400

    fix: separate commit and comment updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 // @ts-check
+
 const debug = require('debug')('cypress-set-github-status')
 const {
   setGitHubCommitStatus,
@@ -58,24 +59,26 @@ function registerPlugin(on, config, options = {}) {
   debug('test commit %s comment id %s', testCommit, commentId)
 
   if (options.owner && options.repo) {
+    const owner = options.owner
+    const repo = options.repo
+
     if (testCommit) {
       debug('have the commit %s', testCommit)
 
-      const owner = options.owner
-      const repo = options.repo
-
       console.log('after finishing the test run will report the results')
       console.log('as a status check %s/%s commit %s', owner, repo, testCommit)
+    }
 
-      const envOptions = {
-        token: options.token,
-      }
-      const context = getContext()
-      debug('context "%s"', context)
+    const envOptions = {
+      token: options.token,
+    }
+    const context = getContext()
+    debug('context "%s"', context)
 
-      on('before:run', async (runResults) => {
-        // put the target repo information into the options
+    on('before:run', async (runResults) => {
+      // put the target repo information into the options
 
+      if (testCommit) {
         const ghOptions = {
           owner,
           repo,
@@ -87,67 +90,71 @@ function registerPlugin(on, config, options = {}) {
         }
 
         await setGitHubCommitStatus(ghOptions, envOptions)
+      }
 
-        if (commentId) {
-          if (runResults.runUrl) {
-            // add a comment with the link to the test run
-            const commentOptions = {
-              owner,
-              repo,
-              comment: Number(commentId),
-              text: `Cypress test run at ${runResults.runUrl}`,
-            }
-            const result = await addOrUpdateComment(commentOptions, envOptions)
-            debug('addOrUpdateComment result %o', result)
-            if (result === 'comment updated') {
-              console.log(
-                'Updated existing comment with ID %d, added link to the test run %s',
-                commentOptions.comment,
-                runResults.runUrl,
-              )
-            }
+      if (commentId) {
+        if (runResults.runUrl) {
+          // add a comment with the link to the test run
+          const commentOptions = {
+            owner,
+            repo,
+            comment: Number(commentId),
+            text: `Cypress test run at ${runResults.runUrl}`,
+          }
+          const result = await addOrUpdateComment(commentOptions, envOptions)
+          debug('addOrUpdateComment result %o', result)
+          if (result === 'comment updated') {
+            console.log(
+              'Updated existing comment with ID %d, added link to the test run %s',
+              commentOptions.comment,
+              runResults.runUrl,
+            )
           }
         }
-      })
+      }
+    })
 
-      on('after:spec', async (spec, results) => {
-        debug('spec finished')
-        debug(spec)
-        debug('results')
-        debug(results)
+    on('after:spec', async (spec, results) => {
+      debug('spec finished')
+      debug(spec)
+      debug('results')
+      debug(results)
 
-        if (commentId) {
-          // maybe there are failed tests that we need to report
-          if (results.stats.failures > 0) {
-            // collect the full titles of the failed tests
-            const failedTestTitles = results.tests
-              .filter((test) => test.state === 'failed')
-              .map((test) => test.title.join(' / '))
-            const text =
-              `🚨 spec ${spec.relative} has ${results.stats.failures} failed ${pluralize('test', results.stats.failures, false)}:\n` +
-              `${failedTestTitles.map((t) => `- ${t}`).join('\n')}\n\n`
-            const commentOptions = {
-              owner,
-              repo,
-              comment: Number(commentId),
-              text,
-            }
-            await addOrUpdateComment(commentOptions, envOptions)
+      if (commentId) {
+        // maybe there are failed tests that we need to report
+        if (results.stats.failures > 0) {
+          // collect the full titles of the failed tests
+          const failedTestTitles = results.tests
+            .filter((test) => test.state === 'failed')
+            .map((test) => test.title.join(' / '))
+          const text =
+            `🚨 spec ${spec.relative} has ${results.stats.failures} failed ${pluralize('test', results.stats.failures, false)}:\n` +
+            `${failedTestTitles.map((t) => `- ${t}`).join('\n')}\n\n`
+          const commentOptions = {
+            owner,
+            repo,
+            comment: Number(commentId),
+            text,
           }
+          await addOrUpdateComment(commentOptions, envOptions)
         }
-      })
+      }
+    })
 
-      on('after:run', async (runResults) => {
-        const status = runResults.totalFailed > 0 ? 'failure' : 'success'
-        const description = `${pluralize(
-          'spec',
-          runResults.runs.length,
-          true,
-        )}: ${runResults.totalPassed} passed, ${runResults.totalFailed} failed, ${
-          runResults.totalPending + runResults.totalSkipped
-        } other tests`
-        const targetUrl = runResults.runUrl || process.env.CIRCLE_BUILD_URL
+    on('after:run', async (runResults) => {
+      const status = runResults.totalFailed > 0 ? 'failure' : 'success'
+      const description = `${pluralize(
+        'spec',
+        runResults.runs.length,
+        true,
+      )}: ${runResults.totalPassed} passed, ${runResults.totalFailed} failed, ${
+        runResults.totalPending + runResults.totalSkipped
+      } other tests`
+      const targetUrl = runResults.runUrl || process.env.CIRCLE_BUILD_URL
 
+      debug('after:run status %s', status)
+
+      if (testCommit) {
         const commitOption = {
           owner,
           repo,
@@ -165,32 +172,32 @@ function registerPlugin(on, config, options = {}) {
           }
           await setCommonStatus(options.commonStatus, commitOption, envOptions)
         }
+      }
 
-        if (commentId) {
-          debug(
-            'posting the final test results message to the commit %s',
-            commentId,
-          )
-          const status = runResults.totalFailed > 0 ? 'failure' : 'success'
-          const description = `${pluralize(
-            'spec',
-            runResults.runs.length,
-            true,
-          )}: ${runResults.totalPassed} passed, ${runResults.totalFailed} failed, ${
-            runResults.totalPending + runResults.totalSkipped
-          } other tests`
+      if (commentId) {
+        debug(
+          'posting the final test results message to the commit %s',
+          commentId,
+        )
+        const status = runResults.totalFailed > 0 ? 'failure' : 'success'
+        const description = `${pluralize(
+          'spec',
+          runResults.runs.length,
+          true,
+        )}: ${runResults.totalPassed} passed, ${runResults.totalFailed} failed, ${
+          runResults.totalPending + runResults.totalSkipped
+        } other tests`
 
-          const text = `The test run has finished with status **${status}**: ${description}\n\n`
-          const commentOptions = {
-            owner,
-            repo,
-            comment: Number(commentId),
-            text,
-          }
-          await addOrUpdateComment(commentOptions, envOptions)
+        const text = `The test run has finished with status **${status}**: ${description}\n\n`
+        const commentOptions = {
+          owner,
+          repo,
+          comment: Number(commentId),
+          text,
         }
-      })
-    }
+        await addOrUpdateComment(commentOptions, envOptions)
+      }
+    })
   }
 }
 
